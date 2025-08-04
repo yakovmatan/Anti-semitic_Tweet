c6d864e754c7df4c1d875da91dc1258f0afc89d1
yakov matan <yakovmatan@gmail.com>
2025-08-04 17:36:07 +0300

    fix main

--- src/main.py
+++ src/main.py
@@ -3,20 +3,22 @@ from src.manager_cleaner import ManagerCleaner
 from src.manager_load_data import ManagerLoadData
 
 
-# Operation of the mangers
-data = ManagerLoadData()
-analyzer = ManagerAnalyzer()
-cleaner = ManagerCleaner()
+def main():
+    data = ManagerLoadData()
+    analyzer = ManagerAnalyzer()
+    cleaner = ManagerCleaner()
 
-#data
-df = data.loading_csv_data()
-df2 = df.copy()
+    df = data.loading_csv_data()
+    df2 = df.copy()
 
-data_analyzed = analyzer.data_analyzed(df)
-analyzer.save_to_json(data_analyzed)
+    data_analyzed = analyzer.data_analyzed(df)
+    analyzer.save_to_json(data_analyzed)
 
-clean_df = cleaner.clean_data(df2)
-cleaner.save_to_csv(clean_df)
+    clean_df = cleaner.clean_data(df2)
+    cleaner.save_to_csv(clean_df)
+
+if __name__ == '__main__':
+    main()
 
 
 
