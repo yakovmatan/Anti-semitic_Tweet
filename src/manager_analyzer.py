7ee74827dcceea4aaae0a8786ee8320e65d2f204
yakov matan <yakovmatan@gmail.com>
2025-08-04 16:24:34 +0300

    create function to save data to json

--- src/manager_analyzer.py
+++ src/manager_analyzer.py
@@ -16,4 +16,10 @@ class manager_analyzer:
         return data_analyzer
 
 
+    @staticmethod
+    def save_to_json(data, path='result/data_analysis.json'):
+        with open(path, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
+
 
